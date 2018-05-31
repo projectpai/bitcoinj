6cfa3fadd28e51008500105fe227b0efd338eee7
Petar Partlov <petar.partlov@upandrunningsoftware.com>
2018-05-31 20:46:46 +0200

    Added sample with raw transaction signing

--- paidemo/src/main/java/com/oben/Main.java
+++ paidemo/src/main/java/com/oben/Main.java
@@ -3,10 +3,18 @@ package com.oben;
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.bind.DatatypeConverter;
+
 import org.bitcoinj.core.Address;
 import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.DumpedPrivateKey;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.ECKey.ECDSASignature;
 import org.bitcoinj.core.InsufficientMoneyException;
 import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionConfidence;
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.wallet.Wallet;
@@ -22,21 +30,46 @@ import asg.cliche.ShellFactory;
 
 public class Main {
 
+	@Command(description = "Create raw transaction without breoadcast and sign it with provided private key")
+	public void rawTx(@Param(name = "address") String toAddress, @Param(name = "amount") String amount,
+			@Param(name = "privateKey") String privateKey) {
+
+		NetworkParameters params = TestNet3Params.get();
+		DumpedPrivateKey dumpedPrivateKey = DumpedPrivateKey.fromBase58(params, privateKey);
+		ECKey key = dumpedPrivateKey.getKey();
+
+		Address to = Address.fromBase58(params, toAddress);
+
+		Transaction tx = new Transaction(params);
+		tx.addOutput(Coin.parseCoin(amount), to);
+
+		tx.getConfidence().setSource(TransactionConfidence.Source.SELF);
+		tx.setPurpose(Transaction.Purpose.USER_PAYMENT);
+
+		Sha256Hash hash = Sha256Hash.wrap(tx.getHashAsString());
+		ECDSASignature sig = key.sign(hash);
+		byte[] encodedBytes = sig.encodeToDER();
+		String signedTransaction = DatatypeConverter.printHexBinary(encodedBytes);
+
+		System.out.println("Raw transaction created: " + tx.getHashAsString());
+		System.out.println("Signed transaction: " + signedTransaction);
+	}
+
 	@Command(description = "Send some amount of coins to provided address")
 	public void send(@Param(name = "address") String toAddress, @Param(name = "amount") String amount) {
-		
+
 		NetworkParameters params = TestNet3Params.get();
 		WalletAppKit kit = new WalletAppKit(params, new File("."), "sendrequest-example");
-		
+
 		kit.startAsync();
 		kit.awaitRunning();
-		
+
 		Coin value = Coin.parseCoin(amount);
 		Address to = Address.fromBase58(params, toAddress);
-		
+
 		try {
 			Wallet.SendResult result = kit.wallet().sendCoins(kit.peerGroup(), to, value);
-			
+
 			System.out.println("coins sent. transaction hash: " + result.tx.getHashAsString());
 			System.out.println("transaction fee: " + result.tx.getFee());
 		} catch (InsufficientMoneyException e) {
